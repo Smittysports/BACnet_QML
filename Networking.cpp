4f67129fb510033575eb5bbb1fd046847c266cb1
Brian Smith <smittysports@yahoo.com>
2025-01-06 12:59:43 -0500

    First communication with BACpypes

--- Networking.cpp
+++ Networking.cpp
@@ -1,7 +1,61 @@
 #include "Networking.h"
+#include <QByteArray>
+#include <QDebug>
 
 namespace BACnet {
 
-Networking::Networking() {}
+Networking::Networking(QObject *parent) :
+    QObject{parent},
+    m_hostAddress("10.0.0.179"),
+    m_port(47808)
+{
+    m_clientSocket = new QUdpSocket(this);
+    m_clientSocket->bind(m_hostAddress, m_port);
+    m_clientSocket->flush();
+    connect(m_clientSocket, SIGNAL(readyRead()), this, SLOT(readyRead()));
+}
+
+void Networking::send()
+{
+    sendWriteAnalogValue();
+    sendReadAnalogValue();
+}
+
+void Networking::sendWriteAnalogValue()
+{
+    // Write 1.0 to the PresentValu of an AnalogValue object
+    unsigned char dat[]={0x81, 0x0a, 0x00, 0x18, 0x01, 0x04, 0x02, 0x05, 0xe8, 0x0f, 0x0c,
+                           0x00, 0x80, 0x00, 0x01, 0x19, 0x55, 0x3e, 0x44, 0x3f, 0x80, 0x00,
+                           0x00, 0x3f};
+    QByteArray datagram=QByteArray::fromRawData((char*)dat,24);
+    qDebug() << "sendWriteAnalogValue: " << datagram << "\n";
+    QHostAddress device {"10.0.0.77"};
+    int devicePort {47809};
+    qint64 bytesSent = m_clientSocket->writeDatagram(datagram, device, devicePort);
+    qDebug() << "Sent " << bytesSent << " bytes\n";
+}
+
+void Networking::sendReadAnalogValue()
+{
+    unsigned char dat[]={0x81, 0x0a, 0x00, 0x11, 0x01, 0x04, 0x02, 0x05, 0x3d, 0x0c, 0x0c, 0x00, 0x80, 0x00, 0x01, 0x19, 0x55};
+
+    QByteArray datagram=QByteArray::fromRawData((char*)dat,17);
+    qDebug() << "sendReadAnalogValue: " << datagram << "\n";
+    QHostAddress device {"10.0.0.77"};
+    int devicePort {47809};
+    qint64 bytesSent = m_clientSocket->writeDatagram(datagram, device, devicePort);
+    qDebug() << "Sent " << bytesSent << " bytes\n";
+}
+
+void Networking::readyRead()
+{
+    QByteArray Buffer;
+    Buffer.resize(m_clientSocket->pendingDatagramSize());
+
+    QHostAddress sender;
+    quint16 senderPort;
+    qint64 bytesReceived = m_clientSocket->readDatagram(Buffer.data(),Buffer.size(),&sender,&senderPort);
+    qDebug() << "Received " << bytesReceived << " bytes\n";
+}
 
 } // namespace BACnet
--- Networking.h
+++ Networking.h
@@ -1,12 +1,86 @@
 #ifndef NETWORKING_H
 #define NETWORKING_H
 
+#include <QUdpSocket>
+#include <QObject>
+
 namespace BACnet {
 
-class Networking
+/**
+ * @brief The Networking class
+ */
+class Networking : QObject
 {
+    Q_OBJECT
+
 public:
-    Networking();
+    explicit Networking(QObject *parent = nullptr);
+
+    void send();
+
+    /** sendWriteAnalogValue
+     *
+     *  AnalogValue:
+     * writeProperty example
+     * 81 0a 00 18 01 04 02 05 e8 0f 0c 00 80 00 01 19 55 3e 44 3f 80 00 00 3f
+     *
+     * BVLC
+     * 81 0a 00 18
+     * - 0x81       = Type: BACnet/IP
+     * - 0x0a       = Function: Original-Unicast-NPDU
+     * - 0x00 0x18  = BVLC-Length: 24 byte BACnet packet length
+     *
+     * NPDU
+     * - 0x01       = Version: ASHRAE 135-1995
+     * - 0x04       = Control: Bit 3 is for 'Expecting Reply'
+     *
+     * APDU
+     * - 0x02       = APDU Type: Confirmed-REQ (4 LSB)
+     *                PDU Flags: 0x2 is for 'Segmented Response Accepted'
+     * - 0x05       = Max Response Segments Accepted: Unspecificed
+     *                Size of Maximum APDU accepted: (4 MSB = 0101) Up to 1476 octets
+     * - 0xe8       = Invoke ID: 232
+     * - 0x0f       = Service Choice: writeProperty
+     * - 0x0c 0x00 0x80 0x00 0x01 = Object Identifier
+     *       - 0x0c     = Context Tag: (MSB = 0)
+     *                    Tag class: bit 4 = 1 'Context specific tag'
+     *                    Length: bit 1 - 3 (4 bytes)
+     *       - 0x00 0x80 0x00 0x01
+     *                  = Object Type: Bits 23 - 32 = 0000 0000 10 'analog-value (2)'
+     *                    Instance Number: Bits 1 - 22 = 1
+     * - 0x19 0x55  = Property Identifier
+     *      - 0x19      = Context Tag: Bit 4 = 1 'Context specific tag'
+     *                    Context tag number: (MSB = 0001 = 1)
+     *                    Length: bit 1 - 3 (1 byte)
+     *      - 0x55      = Property Identifier: present-value (85)
+     * - 0x3e       = Tag class: bit 4 = 1 'Context specific tag'
+     *                Context Tag: (MSB = 3)
+     *                Named tag: bit 1 - 3 'Opening Tag' (6)
+     * - 0x44 0x3f 0x80 0x00 0x00    = Present value
+     *      - 0x44      = Application Tag
+     *                    Tag class: Bit 4 = 0 'Application tag'
+     *                    Application tag number: (MSB = 0100) = 'Real' (4)
+     *                    Length: bit 1 - 3 (4 bytes)
+     *      - 0x3f 0x80 0x00 0x00: real value of 1
+     * - 0x3f       = Tag class: bit 4 = 1 'Context specific tag'
+     *                Context Tag: (MSB = 3)
+     *                Named tag: bit 1 - 3 'Closing Tag' (7)
+     */
+    void sendWriteAnalogValue();
+
+    /** sendReadAnalogValue
+     *
+     *  81 0a 00 11 01 04 02 05 3d 0c 0c 00 80 00 01 19 55
+     */
+    void sendReadAnalogValue();
+
+private:
+    QHostAddress m_hostAddress;
+    int m_port;
+    QUdpSocket* m_clientSocket;
+
+public slots:
+    void readyRead();
 };
 
 } // namespace BACnet
